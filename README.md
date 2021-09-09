e2ab23f31222c68b056ca277cc3cd367708d2996
Chen, Zai-Chun <chenzaichun@gmail.com>
2021-09-09 12:41:31 +0800

    init version

---
+++ README.md
@@ -0,0 +1,14 @@
+# 2021
+关于我的2021的一些记录（从2021-09-09开始）。代码来自 [yihong618](https://github.com/yihong0618/2021l), 我做了自己的修改，感谢 `yihong618`
+
+## 我的数字
+
+<!--START_SECTION:my_number-->
+| Name | Status | Streak | Today? | 
+ | ---- | ---- | ---- | ---- |
+<!--END_SECTION:my_number-->
+
+## 跑步
+
+![](https://raw.githubusercontent.com/chenzaichun/running_page/gh-pages/static/assets/github_2021.svg)
+
---
+++ daily/__init__.py
@@ -0,0 +1,24 @@
+from .cichang import get_cichang_daily
+from .config import MY_CICHANG_URL, MY_DUOLINGO_URL, MY_SHANBAY_URL
+from .duolingo import get_duolingo_daily
+from .from_issues import get_info_from_issue_comments
+from .shanbay import get_shanbay_daily
+
+MY_STATUS_DICT_FROM_API = {
+    # TODO url
+    "扇贝": {"daily_func": get_shanbay_daily, "url": MY_SHANBAY_URL, "unit_str": " (天)"},
+    "多邻国": {
+        "daily_func": get_duolingo_daily,
+        "url": MY_DUOLINGO_URL,
+        "unit_str": " (点)",
+    },
+    "词场": {"daily_func": get_cichang_daily, "url": MY_CICHANG_URL, "unit_str": " (天)"},
+}
+
+MY_STATUS_DICT_FROM_COMMENTS = {
+    "俯卧撑": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
+    "冥想": {"daily_func": get_info_from_issue_comments, "unit_str": " (分钟)"},
+    "早起": {"daily_func": get_info_from_issue_comments, "unit_str": " (天)"},
+    "GTD": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
+    "周记": {"daily_func": get_info_from_issue_comments, "unit_str": " (周)"},
+}
